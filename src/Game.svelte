569956b920c80609b63d79823f5ac53b0696a716
Ben <b.mcgarvey.bm@gmail.com>
2019-09-14 17:30:39 +1000

    initial-commit

---
+++ src/Game.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Board from './Board.svelte';
+</script>
+
+<style>
+  ol {
+    padding-left: 30px;
+  }
+
+  .game {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .game-info {
+    margin-left: 20px;
+  }
+</style>
+
+<div class='game'>
+  <div class='game-board'>
+    <Board />
+  </div>
+  <div class='game-info'>
+    <!-- TODO: status -->
+  <div />
+  <ol>
+    <!-- TODO: moves -->
+  <ol />
+  </div>
+</div>
